c41def9c8bc881bff6bf4f793a9b7efb4bad5b71
NADENDLA JAFAR VALLI <100751484+Jafarvalli@users.noreply.github.com>
2022-05-08 20:03:33 -0700

    Create App.js

---
+++ App.js
@@ -0,0 +1,53 @@
+import React,{useState} from "react";
+import { fetchWeather } from "./fetchweather.js";
+import './App.css';
+
+function App() {
+  const [query,setQuery]=useState('')
+  const [weather,setWeather]=useState({})
+
+
+  const handleSearch=(e)=>{
+    setQuery(e.target.value)
+  }
+
+  const search=async (e)=>{
+    if(e.code === 'Enter'){
+      const data=await fetchWeather(query)
+      setQuery('')
+      setWeather(data)
+    }
+  }
+
+  return (
+    <div className="main">
+      <center>
+      <input value={query} type='text'placeholder="search..." className="search" onChange={handleSearch}
+       onKeyPress={search} />
+       
+       {weather.main && 
+       
+       <div className="city">
+           <div className="city-name">
+               <h2>{weather.name}</h2>
+                <span>{weather.sys.name}</span>
+           
+           <div className="temp">
+             {Math.round(weather.main.temp)}
+             <span>&deg;C</span>
+             </div>
+             
+             <div className="info">
+               <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description}/>
+               <p>{weather.weather[0].description}</p>
+               </div>
+
+           </div>
+         </div>
+}
+</center>
+    </div>
+  );
+}
+
+export default App;
